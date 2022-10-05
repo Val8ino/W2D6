090350ea5af7d1d2e0bcdd22bb2a7e4a48783967
Valeryia Kulikovich <114145653+Val8ino@users.noreply.github.com>
2022-10-05 10:37:38 +0200

    name

---
+++ name.js
@@ -0,0 +1,8 @@
+let myName = "Valeria";
+let myLastName = "Kulikovich";
+let myAge = 28;
+let currentYear = 2022;
+console.log(myName, myLastName);
+console.log("Hello, my name is", myName);
+let myBirthYear = currentYear - myAge;
+console.log("I was born in", myBirthYear);
